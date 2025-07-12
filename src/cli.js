a859ab49b1ab58a4e3254ebc2ad063f00e38f3ef
jlauza <lauzajayson@gmail.com>
2025-07-12 21:04:44 +0800

    move to default dir

--- src/cli.js
+++ src/cli.js
@@ -3,7 +3,7 @@
 const { resolve } = require("path");
 const { create } = require("create-create-app");
 
-const templateRoot = resolve(__dirname, "..", "templates", "default");
+const templateRoot = resolve(__dirname, "..", "templates");
 
 const [, , rawArg] = process.argv;
 const fallbackName = rawArg || "my-app";
@@ -16,7 +16,7 @@ if (cond.includes("--help") || cond.includes("-h")) {
 
 create("dashme", {
   templateRoot,
-  defaultTemplate: ".",
+  defaultTemplate: "default",
   promptForTemplate: false,
   skipPrompts: true,
   skipGitInit: true,
